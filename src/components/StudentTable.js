637e67e522baff0da2d51427c4644b507668440d
NirmalNaveen <nirmalnaveen77@gmail.com>
2022-11-06 10:18:07 -0800

    TSM

--- src/components/StudentTable.js
+++ src/components/StudentTable.js
@@ -19,21 +19,21 @@ function StudentTable() {
           <td>Nirmal</td>
           <td>naveen@gmail</td>
           <td>98076554</td>
-          <td><Button variant="primary" type="Edit">Edit</Button>  <Button variant="primary" type="Delete">Delete</Button></td>
+          <td><Button variant="primary" type="Edit">Edit</Button>  <Button variant="danger" type="Delete">Delete</Button></td>
         </tr>
         <tr>
           <td>2</td>
           <td>Vicky</td>
           <td>vicky@gmail</td>
           <td>97645778</td>
-          <td><Button variant="primary" type="Edit">Edit</Button>  <Button variant="primary" type="Delete">Delete</Button></td>
+          <td><Button variant="primary" type="Edit">Edit</Button>  <Button variant="danger" type="Delete">Delete</Button></td>
         </tr>
         <tr>
           <td>3</td>
           <td>sekar</td>
           <td>sekar@gmail</td>
           <td>905846738</td>
-          <td><Button variant="primary" type="Edit">Edit</Button>  <Button variant="primary" type="Delete">Delete</Button></td>
+          <td><Button variant="primary" type="Edit">Edit</Button>  <Button variant="danger" type="Delete">Delete</Button></td>
         </tr>
       </tbody>
     </Table>
--- src/components/TeacherTable.js
+++ src/components/TeacherTable.js
@@ -18,21 +18,21 @@ function StudentTable() {
           <td>ram</td>
           <td>ram@gmail</td>
           <td>98076554</td>
-          <td><Button variant="primary" type="Edit">Edit</Button>   <Button variant="primary" type="Delete">Delete</Button></td>
+          <td><Button variant="primary" type="Edit">Edit</Button>   <Button variant="danger" type="Delete">Delete</Button></td>
         </tr>
         <tr>
           <td>2</td>
           <td>gopi</td>
           <td>gopi@gmail</td>
           <td>97645778</td>
-          <td><Button variant="primary" type="Edit">Edit</Button>   <Button variant="primary" type="Delete">Delete</Button></td>
+          <td><Button variant="primary" type="Edit">Edit</Button>   <Button variant="danger" type="Delete">Delete</Button></td>
         </tr>
         <tr>
           <td>3</td>
           <td>chandran</td>
           <td>chandran@gmail</td>
           <td>905846738</td>
-          <td><Button variant="primary" type="Edit">Edit</Button>   <Button variant="primary" type="Delete">Delete</Button></td>
+          <td><Button variant="primary" type="Edit">Edit</Button>   <Button variant="danger" type="Delete">Delete</Button></td>
         </tr>
       </tbody>
     </Table>
